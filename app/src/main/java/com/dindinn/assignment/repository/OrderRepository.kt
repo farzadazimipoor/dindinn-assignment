82896d846cd276fb2e6371bbbda82c4be5fe962c
farzad <farzad.azimipoor@gmail.com>
2021-10-09 13:56:16 +0300

    Orders repository documentation added

--- app/src/main/java/com/dindinn/assignment/repository/OrderRepository.kt
+++ app/src/main/java/com/dindinn/assignment/repository/OrderRepository.kt
@@ -10,16 +10,29 @@ import com.google.gson.reflect.TypeToken
 import javax.inject.Inject
 import javax.inject.Singleton
 
+/**
+ * Orders Repository
+ * This class contains all methods that are needed for read orders data
+ */
 @Singleton
 class OrderRepository @Inject constructor() {
+
+    /**
+     * Read orders json file, parse Json and return list of Order items
+     * @return List of order items
+     */
     fun fetchOrdersList(context: Context): List<OrderItemModel> {
-        val ordersJsonData = Utils.getJsonDataFromAsset(context, "orders.json")
-        Log.i("data", ordersJsonData ?: "Json data is empty")
-        if (ordersJsonData.isNullOrEmpty()) return listOf()
-        val ordersResultType = object : TypeToken<OrdersResultModel>() {}.type
-        val result: OrdersResultModel = Gson().fromJson(ordersJsonData, ordersResultType)
-        if (result.status.success) {
-            return result.data
-        } else throw Exception(result.status.message)
+        try {
+            val ordersJsonData = Utils.getJsonDataFromAsset(context, "orders.json")
+            Log.i("data", ordersJsonData ?: "Json data is empty")
+            if (ordersJsonData.isNullOrEmpty()) return listOf()
+            val ordersResultType = object : TypeToken<OrdersResultModel>() {}.type
+            val result: OrdersResultModel = Gson().fromJson(ordersJsonData, ordersResultType)
+            if (result.status.success) {
+                return result.data
+            } else throw Exception(result.status.message)
+        } catch (e: Exception) {
+            throw e
+        }
     }
 }
